40a3f9d26cc12faaeae71a8d46ebc9514125d6e7
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-04-02 21:03:17 -0400

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function(driver) {
+      for (var i = 0; i < letters.length; i++) {
+        if (driver.slice(i, i + 1).toLowerCase() !== letters[i].toLowerCase()) {
+          return false;
+        }
+      }
+      return true;
+    });
+  }
+
+function matchName(drivers, str) {
+    return drivers.filter(driver => driver.name === str)
+}
